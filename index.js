f767369cc87568cd15a439ef84388c0cc7d45f06
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-29 22:17:52 +0300

    routing with handler funcions

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const http = require('http');
 
-const homePage = `
-<h1>Welcome to our page</h1>
-`
-const aboutPage = `
-<h1>Welcome to about page</h1>
-`
+
+function homePage(req,res){
+    res.write(html('<h1>Welcome to our page</h1>', 'Home page'))
+    res.end()
+}
+
+function aboutPage(req,res){
+    res.write(html('<h1>Welcome to about page</h1>', 'About page'))
+    res.end()
+}
+
+function defaultPage(req,res){
+    res.statusCode = 404;
+    res.writeHead(404, [
+        'Content-Type', 'plain/text'
+    ])
+    res.write('Page not found')
+}
 
 const routes = {
     '/': homePage,
@@ -13,37 +25,35 @@ const routes = {
 }
 
 //pass a handler as anonymous function
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, response) => {
     //in the request we have the information for the client behaviour.
 
     //automatically parsed to lowercase thanks to the http module.
-    console.log(req.headers)
-    console.log(req.method)
-    console.log(req.url)
+    console.log(request.headers)
+    console.log(request.method)
+    console.log(request.url)
     
 
-    const url = new URL(req.url, `http://${req.headers.host}`)
-    const page = routes[url.pathname]
+    const url = new URL(request.url, `http://${request.headers.host}`)
+    const handlerFunc = routes[url.pathname]
 
     console.log(url)
-    if(page !==undefined){
-        res.write(html(page))
-        res.end()
+    if(typeof handlerFunc === 'function'){
+        handlerFunc(request,response)
     }else{
-        res.statusCode = 404;
-        res.writeHead(404, [
-            'Content-Type', 'plain/text'
-        ])
-        res.write('Page not found')
+        defaultPage(request,response)
     }
 
     //directly write on the stream
 })
 
-function html(body) {
+function html(body, title='Demo site') {
     return`
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<title> ${title} </title>
+</head>
         <body>
     <nav>
             <ul>
