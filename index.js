c4b9456d32fafa3600259de45cf701b411ab9bca
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-29 22:06:07 +0300

    basic routing from URL class

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const http = require('http')
+const http = require('http');
 
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<body>
- <h1>Hello world</h1>
-</body>
-</html>`
+const homePage = `
+<h1>Welcome to our page</h1>
+`
+const aboutPage = `
+<h1>Welcome to about page</h1>
+`
 
 //pass a handler as anonymous function
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     //in the request we have the information for the client behaviour.
-    
+
     //automatically parsed to lowercase thanks to the http module.
     console.log(req.headers)
     console.log(req.method)
     console.log(req.url)
-    console.log('request received')
-    
-    if(req.url === '/'){
-        res.write(html)
+
+
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    console.log(url)
+    if (url.pathname === '/') {
+        res.write(html(homePage))
         res.end();//this puts an empty row on the last response row
-    }else {
+    } else if (url.pathname === '/about') {
+        res.write(html(aboutPage))
+        res.end()
+    } else {
         res.statusCode = 404;
         res.writeHead(200, [
             'Content-Type', 'text/plain'
@@ -29,8 +33,26 @@ const server = http.createServer((req,res)=>{
         res.write('Wrong page');
         res.end()
     }
-    
+
     //directly write on the stream
 })
 
-server.listen(3000)
+function html(body) {
+    return`
+<!DOCTYPE html>
+<html lang="en">
+        <body>
+    <nav>
+            <ul>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            </ul>
+    </nav>
+     ${body}
+         </body>
+</html>`
+
+}
+
+server.listen(3000);
+
